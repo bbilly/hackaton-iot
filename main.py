6cf23efcdc9fac43f2c0391ecab6548152d4c7ee
Perraudeau <victor@perraudeau.net>
2016-04-25 17:35:02 +0200

    Mise à jour ajout du service /synthesis
    Pour pouvoir fonctionner correctement, il faudrait utiliser le design pattern producer-consumer pour crée une zone tampon.

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 #!flask/bin/python
 from flask import Flask
 from flask import request
+import sqlite3
 
 app = Flask(__name__)
 
+#!Création ou réutilisation de la bdd
+conn = sqlite3.connect('rest.db')
+
+#!Création de la table
+cursor = conn.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS data(id TEXT,timestamp TEXT,sensorType INTEGER,value INTEGER)""")
+conn.commit()
+
+messages = []
+nbMessageInArray = 0
+
 @app.route('/messages', methods=['POST'])
 def save_messages():
+
 	id = request.json["id"]
 	timestamp = request.json["timestamp"]
 	sensorType = request.json["sensorType"]
 	value = request.json["value"]
 
-	print "json : "+timestamp
+	#!cursor.execute("INSERT INTO data VALUES (id, timestamp, sensorType, value)")
+	#!conn.commit()
+	return "OK"
+
+@app.route('/synthesis', methods=['GET'])
+def synthesis():
 	return "OK"
 
 if __name__ == '__main__':
