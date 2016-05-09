656047fdaa9637c1672dec79a71abf5370197ec8
hackaton <hackaton@localhost.localdomain>
2016-05-09 21:47:09 +0200

    victor

--- settings.py
+++ settings.py
@@ -1,2 +1,7 @@
-CELERY_BROKER_URL='sqla+sqlite:///rest.db'
-CELERY_RESULT_BACKEND='sqla+sqlite:///rest.db'
+#CELERY_BROKER_URL='sqla+sqlite:///rest.db'
+#CELERY_RESULT_BACKEND='sqla+sqlite:///rest.db'
+BROKER_URL = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
