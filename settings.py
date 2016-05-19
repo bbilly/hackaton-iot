9ffff832a7ee86b97da167bf70dbaa772cab4e60
Baptiste BILLY <hackaton@localhost.localdomain>
2016-05-19 17:57:26 +0200

    maj 19/05

--- settings.py
+++ settings.py
@@ -1,7 +1,4 @@
-#CELERY_BROKER_URL='sqla+sqlite:///rest.db'
-#CELERY_RESULT_BACKEND='sqla+sqlite:///rest.db'
-BROKER_URL = 'redis://localhost:6379/0'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-
+CELERY_BROKER_URL = 'sqla+sqlite:///rest.db'
+CELERY_RESULT_BACKEND = 'sqla+sqlite:///rest.db'
+DATABASE = 'rest.db'
+NB_MSG_TO_COMMIT = 1000
