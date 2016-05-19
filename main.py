d6fa7bb7673d11d46c59746d249a8a2496f0852d
Baptiste Billy <baptiste.billy@gmail.com>
2016-05-19 17:59:55 +0200

    update

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 #!flask/bin/python
 # coding: utf-8
-<<<<<<< HEAD
 import collections
 from flask import Flask, request
-=======
-#Seulement pour debug
-from __future__ import print_function
-import collections
-from flask import Flask, request, jsonify
->>>>>>> 61efaaf8a7a72152166c113cbeac8c5b417cd3d7
 from celery import Celery
 from dateutil.parser import parse
 from dateutil.relativedelta import *
 from datetime import datetime
-<<<<<<< HEAD
 from decimal import *
 import os
 import sqlite3
@@ -22,19 +14,6 @@ import urllib.parse
 import json
 
 app = Flask(__name__)
-=======
-import os
-import sqlite3
-import settings
-import functools
-import urllib.parse
-import sys
-import time
-import json
-
-app = Flask(__name__)
-app.debug = True
->>>>>>> 61efaaf8a7a72152166c113cbeac8c5b417cd3d7
 
 #Permet de garder l'ordre des dictionnaire lors de la jsonify du dictionnaire des données
 app.config["JSON_SORT_KEYS"] = False
@@ -55,7 +34,7 @@ cursor = conn.cursor()
 #Désynchronisation pour gagner en performance, perte de stabilité en contrepartie.
 cursor.execute("""PRAGMA synchronous=OFF""")
 #Création de la table
-cursor.execute("""CREATE TABLE IF NOT EXISTS data(id INTEGER,timestamp TIMESTAMP,sensorType INTEGER,value INTEGER)""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS data(id TEXT,timestamp TEXT,sensorType INTEGER,value INTEGER)""")
 conn.commit()
 
 #Création d'une liste pour stocker les données en mémoire pour éviter de commit les données trop souvent
@@ -79,15 +58,12 @@ def make_celery(app):
 celery = make_celery(app)
 
 
-<<<<<<< HEAD
 def mean(listValues):
     if (listValues != []):
         result = Decimal(sum(listValues)) / len(listValues)
         return result.quantize(Decimal('.01'), rounding=ROUND_HALF_UP)
 
 
-=======
->>>>>>> 61efaaf8a7a72152166c113cbeac8c5b417cd3d7
 #Cette fonction permet d'ajouter la durée au RF3339 de début et retourne
 #une RFC3339 de fin au bont format.
 def get_end_timestamp(duration, begin_timestamp):
@@ -101,11 +77,7 @@ def get_end_timestamp(duration, begin_timestamp):
             ret = datetime.strptime(str(end_date).split("+")[0], '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%S.%f%z')
         except:
             raise
-<<<<<<< HEAD
     return (str(ret) + "Z")
-=======
-    return end_date.timestamp()
->>>>>>> 61efaaf8a7a72152166c113cbeac8c5b417cd3d7
 
 
 @celery.task(name="main.add")
@@ -123,16 +95,11 @@ def add(dataList):
 def get(begin_timestamp, duration):
     #On commit la liste en base avant de faire le get afin de s'assurer de la concordande des données
     add(dataList)
-<<<<<<< HEAD
-=======
-    time.sleep(1)
->>>>>>> 61efaaf8a7a72152166c113cbeac8c5b417cd3d7
     conn = sqlite3.connect(conn_uri)
     cursor = conn.cursor()
     dataSet = []
 
     #Création de la date de fin, le timestamp RFC3339 de fin est début_timestamp plus la durée
-<<<<<<< HEAD
     begin = str(begin_timestamp)
     end = get_end_timestamp(duration, begin_timestamp)
     cursor.execute("""SELECT DISTINCT sensorType FROM data WHERE timestamp BETWEEN ? AND ? ORDER BY sensorType""", (begin, end))
@@ -145,23 +112,6 @@ def get(begin_timestamp, duration):
         maxValue = max(int(maxValue) for maxValue in valuesBysensorType)
         #meanValue = functools.reduce(lambda x, y: x + y, valuesBysensorType) / len(valuesBysensorType)
         meanValue = mean(valuesBysensorType)
-=======
-    begin = parse(begin_timestamp).timestamp()
-    end = get_end_timestamp(duration, begin_timestamp)
-    print("Begin : " + str(begin), file=sys.stderr)
-    print("End : " + str(end), file=sys.stderr)
-    cursor.execute("""SELECT sensorType FROM data GROUP BY sensorType""")
-    sensorTypes = [row[0] for row in cursor.fetchall()]
-
-    for sensorType in sensorTypes:
-        a = int(sensorType)
-        cursor.execute("SELECT value FROM data WHERE sensorType=?", (a,))
-        valuesBysensorType = [row[0] for row in cursor.fetchall()]
-        minValue = min(int(minValue) for minValue in valuesBysensorType)
-        maxValue = max(int(maxValue) for maxValue in valuesBysensorType)
-        meanValue = functools.reduce(lambda x, y: x + y, valuesBysensorType) / len(valuesBysensorType)
-        meanValue = "%.2f" % meanValue
->>>>>>> 61efaaf8a7a72152166c113cbeac8c5b417cd3d7
 
         data = collections.OrderedDict()
         data['sensorType'] = int(sensorType)
@@ -169,13 +119,8 @@ def get(begin_timestamp, duration):
         data['maxValue'] = int(maxValue)
         data['mediumValue'] = float(meanValue)
         dataSet.append(data)
-<<<<<<< HEAD
 
     return json.dumps(dataSet)
-=======
-    
-    return json.dumps(dataSet, separators=(',', ': '))
->>>>>>> 61efaaf8a7a72152166c113cbeac8c5b417cd3d7
 
 
 #Service REST d'acquisition de messages provenant d'objets connectés
